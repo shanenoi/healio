da6cfe6207a50be701ce71821862236538ac13e0
Shánenoi <shanenoi.org@gmail.com>
2023-07-11 09:39:30 +0700

    implememt logic and reformat project

--- src/components/MedicalRegisterContainer.tsx
+++ src/components/MedicalRegisterContainer.tsx
@@ -1,19 +1,22 @@
-import { type FunctionComponent } from 'react'
+import { type FunctionComponent, type MouseEventHandler } from 'react'
 
-interface EmployeeDataContainerType {
+interface EmployeeInfoContainerType {
   maskGroup?: string
   rightIcon?: string
   xRegular?: string
   leftIcon?: string
   rightIcon1?: string
+  xRegularClick?: MouseEventHandler<HTMLImageElement>
 }
 
-const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
+// todo: add validate for each input
+const MedicalRegisterContainer: FunctionComponent<EmployeeInfoContainerType> = ({
   maskGroup,
   rightIcon,
   xRegular,
   leftIcon,
-  rightIcon1
+  rightIcon1,
+  xRegularClick
 }) => {
   return (
         <div
@@ -21,7 +24,7 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
             <div
                 className="self-stretch w-[636px] flex flex-col py-8 px-0 box-border items-center justify-start gap-[32px]">
                 <div className="self-stretch flex flex-row items-start justify-between text-blue-blue-400">
-                    <img className="relative w-6 h-6" alt=""/>
+                    {/* <img className="relative w-6 h-6" alt="" /> */}
                     <div className="flex-1 flex flex-col items-center justify-center gap-[16px]">
                         <img
                             className="relative w-[100px] h-[100px]"
@@ -37,10 +40,10 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                             <img className="relative w-6 h-6 hidden" alt="" src={rightIcon}/>
                         </div>
                     </div>
-                    <img className="relative w-6 h-6" alt="" src={xRegular}/>
+                    <img className="relative w-6 h-6 cursor-button" alt="" src={xRegular} onClick={xRegularClick}/>
                 </div>
                 <div className="self-stretch flex flex-col items-start justify-start gap-[16px] text-left text-base">
-                    <div className="relative font-medium">Thông tin nhân viên</div>
+                    <div className="relative font-medium">Thông tin bệnh nhân</div>
                     <div
                         className="self-stretch flex flex-col items-start justify-start gap-[16px] text-xs text-grey-grey-900-p">
                         <div className="self-stretch flex flex-row items-start justify-start gap-[16px]">
@@ -61,7 +64,13 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                             alt=""
                                             src="/left-icon.svg"
                                         />
-                                        <div className="flex-1 relative leading-[150%]"> A</div>
+                                        <div className="flex-1 relative leading-[150%]">
+                                            <input
+                                                type="text"
+                                                className={'input'}
+                                                placeholder={'Văn A'}
+                                            />
+                                        </div>
                                         <img
                                             className="relative w-[22px] h-[22px] hidden"
                                             alt=""
@@ -97,7 +106,11 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                             src="/left-icon.svg"
                                         />
                                         <div className="flex-1 relative leading-[150%]">
-                                            Nguyen Van
+                                            <input
+                                                type="text"
+                                                className={'input'}
+                                                placeholder={'Nguyễn'}
+                                            />
                                         </div>
                                         <img
                                             className="relative w-[22px] h-[22px] hidden"
@@ -136,7 +149,11 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                             src="/left-icon.svg"
                                         />
                                         <div className="flex-1 relative leading-[150%]">
-                                            0123456789
+                                            <input
+                                                type="number"
+                                                className={'input no-arrows'}
+                                                placeholder={'0123456789'}
+                                            />
                                         </div>
                                         <img
                                             className="relative w-[22px] h-[22px] hidden"
@@ -173,7 +190,11 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                             src="/left-icon.svg"
                                         />
                                         <div className="flex-1 relative leading-[150%]">
-                                            benhvientunhan@gmail.com
+                                            <input
+                                                type="email"
+                                                className={'input'}
+                                                placeholder={'email@gmail.com'}
+                                            />
                                         </div>
                                         <img
                                             className="relative w-[22px] h-[22px] hidden"
@@ -205,20 +226,27 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                 <div
                                     className="self-stretch rounded-lg bg-monochrome-white box-border h-10 flex flex-row py-2 pr-2 pl-4 items-center justify-start gap-[6px] text-sm text-grey-grey-300-s border-[1px] border-solid border-grey-grey-40-t">
                                     <div className="flex-1 relative leading-[150%] flex items-center h-4">
-                                        Chọn loại khám bệnh
+                                        <select defaultValue={'Chọn loại khám bệnh'}
+                                                className={'input select-input flex-1 relative leading-[150%] flex items-center h-7'}>
+                                            <option className={'select-input-option'} value="" disabled>Chọn loại khám
+                                                bệnh
+                                            </option>
+                                            <option className={'select-input-option'} value="Orange">Orange</option>
+                                            <option className={'select-input-option'} value="Radish">Radish</option>
+                                            <option className={'select-input-option'} value="Cherry">Cherry</option>
+                                        </select>
                                     </div>
-                                    <div className="h-5 flex flex-col py-0.5 px-0 box-border items-start justify-start">
-                                        <div
-                                            className="flex-1 relative box-border w-px border-r-[1px] border-solid border-grey-grey-40-t"/>
-                                    </div>
-                                    <img
-                                        className="relative w-6 h-6"
-                                        alt=""
-                                        src="/caretdown.svg"
-                                    />
+                                    {/* <div className="h-5 flex flex-col py-0.5 px-0 box-border items-start justify-start"> */}
+                                    {/*  <div className="flex-1 relative box-border w-px border-r-[1px] border-solid border-grey-grey-40-t" /> */}
+                                    {/* </div> */}
+                                    {/* <img */}
+                                    {/*  className="relative w-6 h-6" */}
+                                    {/*  alt="" */}
+                                    {/*  src="/caretdown.svg" */}
+                                    {/* /> */}
                                 </div>
                             </div>
-                            <div className="w-[310px] flex flex-col items-start justify-start gap-[8px]">
+                            <div className="w-[310px] flex flex-col items-start justify-start gap-[6px]">
                                 <div className="flex flex-row items-start justify-start gap-[2px]">
                                     <div className="relative leading-[150%]">Hẹn ngày khám</div>
                                     <div
@@ -226,11 +254,48 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                         *
                                     </div>
                                 </div>
-                                <div className="relative w-14 h-[26px]">
+                                <div className="w-[310px] flex flex-col items-start justify-start">
+                                    <div className="hidden flex-row items-start justify-start gap-[2px]">
+                                        <div className="relative leading-[150%]">Hẹn ngày khám</div>
+                                        <div
+                                            className="relative text-xl leading-[24px] font-semibold font-mobile-body-subtitle-2 text-red-red-400 hidden">
+                                            *
+                                        </div>
+                                    </div>
                                     <div
-                                        className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] rounded-81xl bg-blue-blue-400"/>
-                                    <div
-                                        className="absolute h-[76.92%] w-[35.71%] top-[11.54%] right-[5.36%] bottom-[11.54%] left-[58.93%] rounded-[50%] bg-monochrome-white"/>
+                                        className="self-stretch flex flex-col items-start justify-start gap-[4px] text-center text-sm">
+                                        <div
+                                            className="self-stretch rounded-3xs bg-monochrome-white box-border h-[41px] flex flex-row py-0 px-4 items-center justify-start gap-[4px] border-[1px] border-solid border-grey-grey-40-t">
+                                            <img
+                                                className="relative w-[22px] h-[22px] hidden"
+                                                alt=""
+                                                src="/left-icon.svg"
+                                            />
+                                            <div className="flex flex-row items-center justify-start gap-[1px]">
+                                                <div className="relative leading-[150%]">03</div>
+                                                <div className="relative leading-[150%]">/</div>
+                                                <div className="relative leading-[150%]">10</div>
+                                                <div className="relative leading-[150%]">/</div>
+                                                <div className="relative leading-[150%]">2022</div>
+                                            </div>
+                                            <img
+                                                className="relative w-[22px] h-[22px] hidden"
+                                                alt=""
+                                                src="/left-icon.svg"
+                                            />
+                                        </div>
+                                        <div
+                                            className="self-stretch hidden flex-row py-0 px-3 items-end justify-start gap-[4px] text-left text-red-red-400">
+                                            <img
+                                                className="relative w-5 h-5"
+                                                alt=""
+                                                src="/notice-icon.svg"
+                                            />
+                                            <div className="flex-1 relative leading-[150%]">
+                                                Allert
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -252,7 +317,11 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                                         src="/left-icon.svg"
                                     />
                                     <div className="flex-1 relative leading-[150%]">
-                                        Hãy mô tả về tình trạng của bạn
+                                        <input
+                                            type="text"
+                                            placeholder={'Hãy mô tả về tình trạng của bạn'}
+                                            className={'input flex-1 relative  w-[100%]'}
+                                        />
                                     </div>
                                     <img
                                         className="relative w-[22px] h-[22px] hidden"
@@ -448,7 +517,7 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
                 <div
                     className="rounded-xl bg-blue-blue-300 w-[636px] h-12 flex flex-row py-2 px-4 box-border items-center justify-center gap-[8px] text-monochrome-white">
                     <img className="relative w-7 h-7 hidden" alt="" src={leftIcon}/>
-                    <div className="relative leading-[150%] uppercase font-medium">
+                    <div className="relative leading-[150%] uppercase font-medium cursor-button">
                         Đăng ký khám bệnh
                     </div>
                     <img className="relative w-7 h-7 hidden" alt="" src={rightIcon1}/>
@@ -458,4 +527,4 @@ const EmployeeDataContainer: FunctionComponent<EmployeeDataContainerType> = ({
   )
 }
 
-export default EmployeeDataContainer
+export default MedicalRegisterContainer
