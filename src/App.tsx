eac50d9bea505617b97758b70a0bb338dd1ab9ab
Shánenoi <shanenoi.org@gmail.com>
2023-07-17 08:43:48 +0700

    reformat app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,66 @@
 import LogIn from './pages/LogIn'
 import MedicalRegister from './pages/MedicalRegister'
 import TimesheetDoctor from './pages/TimesheetDoctor'
-import { Route, Routes, useLocation, useNavigationType } from 'react-router-dom'
-import { useEffect } from 'react'
+import {Route, Routes, useLocation, useNavigationType} from 'react-router-dom'
+import {useEffect} from 'react'
 
-function App () {
-  const action = useNavigationType()
-  const location = useLocation()
-  const pathname = location.pathname
+function App() {
+    const action = useNavigationType()
+    const location = useLocation()
+    const pathname = location.pathname
 
-  useEffect(() => {
-    if (action !== 'POP') {
-      window.scrollTo(0, 0)
-    }
-  }, [action, pathname])
+    useEffect(() => {
+        if (action !== 'POP') {
+            window.scrollTo(0, 0)
+        }
+    }, [action, pathname])
 
-  useEffect(() => {
-    let title = ''
-    let metaDescription = ''
+    useEffect(() => {
+        let title = ''
+        let metaDescription = ''
 
-    switch (pathname) {
-      case '/':
-        title = ''
-        metaDescription = ''
-        break
-      case '/medical-register':
-        title = 'Medical Register'
-        metaDescription = ''
-        break
-      case '/log-in':
-        title = 'LogIn'
-        metaDescription = ''
-        break
-      case '/timesheet-doctor':
-        title = 'Timesheet Doctor'
-        metaDescription = ''
-        break
-    }
+        switch (pathname) {
+            case '/':
+                title = ''
+                metaDescription = ''
+                break
+            case '/medical-register':
+                title = 'Medical Register'
+                metaDescription = ''
+                break
+            case '/log-in':
+                title = 'LogIn'
+                metaDescription = ''
+                break
+            case '/timesheet-doctor':
+                title = 'Timesheet Doctor'
+                metaDescription = ''
+                break
+        }
 
-    if (title !== '') {
-      document.title = title
-    }
+        if (title !== '') {
+            document.title = title
+        }
 
-    if (metaDescription !== '') {
-      const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
-        'head > meta[name="description"]'
-      )
-      if (metaDescriptionTag != null) {
-        metaDescriptionTag.content = metaDescription
-      }
-    }
-  }, [pathname])
+        if (metaDescription !== '') {
+            const metaDescriptionTag: HTMLMetaElement | null = document.querySelector(
+                'head > meta[name="description"]'
+            )
+            if (metaDescriptionTag != null) {
+                metaDescriptionTag.content = metaDescription
+            }
+        }
+    }, [pathname])
 
-  // login -> login
-  // medical-register -> medical-register7
-  return (
-      <Routes>
-        <Route path="/log-in" element={<LogIn/>}/>
-        <Route path="/medical-register" element={<MedicalRegister/>}/>
-        <Route path="/timesheet-doctor" element={<TimesheetDoctor />} />
-      </Routes>
-  )
+    // login -> login
+    // medical-register -> medical-register7
+    return (
+        <Routes>
+            <Route path="/log-in" element={<LogIn/>}/>
+            <Route path="/medical-register" element={<MedicalRegister/>}/>
+            <Route path="/timesheet-doctor" element={<TimesheetDoctor/>}/>
+        </Routes>
+    )
 }
 
 export default App
